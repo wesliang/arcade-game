9b691186c27f9c12df9b79b2e08e3e381c7d9a52
wesliang <wesliang92@gmail.com>
2017-08-02 09:29:32 -0400

    Updated readme

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Enemies our player must avoid
+// Enemy object initialize stats
 var Enemy = function(x, y, speed) {
     // The image/sprite for our enemies
     this.sprite = 'images/enemy-bug.png';
@@ -17,16 +17,21 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += (this.speed * dt);
 
+    //allow enemy objects to travel a bit before appearing
     if (this.x > 600) {
-      this.x = -100;
+      this.x = -200;
     }
 
-    //if player is near any enemy objects within a certain parameter
+    //detects collision by padding their locations and reduce score if occurs
     if ((player.x < this.x + 80) && (player.x > this.x - 80) && (player.y < this.y + 10) && (player.y > this.y - 10)) {
       player.score--;
       player.x = 200;
       player.y = 380;
-      if (player.score < 0) {player.score = 0;}
+
+      //prevent negative scores
+      if (player.score < 0) {
+        player.score = 0;
+      }
     }
 };
 
@@ -35,9 +40,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// player initialize stats
 var Player = function(x, y, score) {
   this.sprite = 'images/char-boy.png';
   this.x = x;
@@ -46,31 +49,30 @@ var Player = function(x, y, score) {
 };
 
 Player.prototype.update = function() {
+  //if player touches water, reset and increase score
   if (this.y < 50) {
     this.x = 200;
     this.y = 380;
     this.score++;
   }
 
+  //win condition
+  if (this.score === 5) {
 
+  }
 };
-
-// Player.prototype.score = function(score) {
-//   this.score = score;
-// };
 
 Player.prototype.render = function() {
+  //score board and player sprite init
   ctx.font ="20px Georgia";
   ctx.fillStyle = "pink";
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
   ctx.fillText("Score: " + this.score, 420, 80);
+
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
-
 Player.prototype.handleInput = function(allowedKeys) {
-  //only move if there's space to
+  //moves player and prevents offscreen movement
   if (this.x > 50) {
     if (allowedKeys === 'left') {
       this.x -= 100;
@@ -95,16 +97,17 @@ Player.prototype.handleInput = function(allowedKeys) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var enemy1 = new Enemy(0, 55, 500);
-var enemy2 = new Enemy(0, 140, 300);
-var enemy3 = new Enemy(-100, 140, 200);
-var enemy4 = new Enemy(-100, 220, 300);
-var enemy5 = new Enemy(0, 220, 150);
+var enemy1 = new Enemy(-100, 55, 400);
+var enemy2 = new Enemy(-250, 140, 300);
+var enemy3 = new Enemy(-200, 140, 200);
+var enemy4 = new Enemy(-100, 220, 250);
+var enemy5 = new Enemy(-50, 220, 150);
 
+//our player obj
 var player = new Player(200, 380, 0);
 
+//enemy objects
 var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5];
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -115,6 +118,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
